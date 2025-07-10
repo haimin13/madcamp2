bf231e207b066589131f2ac7674511ba80fea8a2
haimin13 <gkals862@naver.com>
2025-07-11 01:25:53 +0900

    로컬에서 mariadb 이용해 db연결 테스트

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,16 @@
+require('dotenv').config();
 const express = require('express');
+const pool = require('./config/mariadb');
 
-const app = express()
+const app = express();
 
-app.get('/', (req,res) => {
-    res.send('Hello World!')
+app.get('/', async (req, res) => {
+    try {
+        const [rows] = await pool.query('SELECT * FROM users');
+        res.json(rows);
+    } catch (err) {
+        res.status(500).send("db 오류: " + err.message);
+    } 
 });
 
 module.exports = app;
