cd6156c05e95a8376ed45517c51e66ef2fda7f15
haimin13 <gkals862@naver.com>
2025-07-12 14:29:58 +0900

    server: change cors options

--- server/src/app.js
+++ server/src/app.js
@@ -17,8 +17,6 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true }));
-
-app.use(express.json());
 app.use('/api', routes);
 
 app.get('/', async (req, res) => {
