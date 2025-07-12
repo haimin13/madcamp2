77418e99ba86431eac5803a7921547f1f034ddd0
haimin13 <gkals862@naver.com>
2025-07-12 18:35:30 +0900

    game 실행가능

--- server/src/app.js
+++ server/src/app.js
@@ -1,13 +1,28 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const routes = require('./routes');
 
 const app = express();
 
-
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+    if (req.url.endsWith('.wasm')) {
+        res.type('application/wasm');
+    } else if (req.url.endsWith('.data')) {
+        res.type('application/octet-stream');
+    } else if (req.url.endsWith('.js')) {
+        res.type('application/javascript');
+    } else if (req.url.endsWith('.gz')) {
+        res.set('Content-Encoding', 'gzip');
+    }
+    next();
+});
+app.use('/game', express.static(path.join(__dirname, './GameBuild')));
+
 app.use('/api', routes);
 
 app.get('/game', (req, res) => {
